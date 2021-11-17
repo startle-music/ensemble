c0561d4fae69043ff0a46c68164a219e2a54997a
Ted Selig <eselig@startleint.com>
2021-11-17 10:18:05 -0500

    add theme

---
+++ src/style/theme.js
@@ -0,0 +1,13 @@
+const theme = {
+    colors: {
+        simplyRed: '#e83948',
+        orangeCrush: '#f7823b',
+        yellowSubmarine: '#f9bd45',
+        fadeToGrey: '#b7c9db',
+        mrBlueSky: '#b3d5e1',
+        paintItBlack: '#1C2229',
+        blueMonday: '#30a4c5',
+        greenDay: '#7cc530',
+        purpleRain: '#6033b9',
+    },
+};
