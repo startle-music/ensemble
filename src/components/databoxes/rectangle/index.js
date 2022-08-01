d58852bdb0d9f7b99183b6bda816f5eca8f1943e
Ted Selig <eselig@startleint.com>
2022-08-01 08:48:11 -0400

    fix props

--- src/components/databoxes/rectangle/index.js
+++ src/components/databoxes/rectangle/index.js
@@ -12,7 +12,7 @@ const Children = styled.div`
     margin-top:.25rem;
 `
 
-const Rectangle = ({text="",children}) => {
-    return <Wrap><Text type="small">{text}</Text><Children>{children}</Children></Wrap>
+const Rectangle = ({text="",children, ...rest}) => {
+    return <Wrap {...rest}><Text type="small">{text}</Text><Children>{children}</Children></Wrap>
 }
 export default Rectangle;
