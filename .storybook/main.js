aa0312e7ceb0d1d3119305e13972aaf1d5a8980f
Anthony Totton <novocast7@gmail.com>
2023-06-12 13:22:40 +0100

    upgrades dependancies, inc storybook. updates peer deps for shared resources

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,14 +1,16 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ],
+  "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-mdx-gfm"],
   staticDirs: ['../public'],
   core: {
-    disableTelemetry: true, // 👈 Disables telemetry
+    disableTelemetry: true // 👈 Disables telemetry
   },
-}
+
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {}
+  },
+  docs: {
+    autodocs: true
+  }
+};
