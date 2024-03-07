55137d06ea0f83929a2d66485752d3d9b3b527e4
Ben Herbertson <bherbertson@startleint.com>
2024-03-07 09:24:12 +0000

    Build

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     build: {
-      lib: {
-        entry: 'src/index.jsx',
-        name: 'Ensemble',
-        fileName: (format) => `ensemble.${format}.js`
-      },
+        lib: {
+            entry: 'src/index.jsx',
+            name: 'Ensemble',
+            fileName: format => `ensemble.${format}.js`
+        }
     },
-    plugins: [react()],
-})
+    plugins: [react()]
+});
